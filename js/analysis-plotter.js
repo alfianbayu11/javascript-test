51f41c7dbfb0982608ed9dbf462808a050c12fd0
alfianbayu11 <alfianbayu11@gmail.com>
2024-12-24 15:37:52 +0700

    edit 2

--- js/analysis-plotter.js
+++ js/analysis-plotter.js
@@ -45,7 +45,9 @@ class AnalysisPlotter {
                         title: {
                             display: true,
                             text: 'Position (m)'
-                        }
+                        },
+                        // Set max X based on condition and spans
+                        max: this.getMaxX(data)
                     },
                     y: {
                         title: {
@@ -64,10 +66,30 @@ class AnalysisPlotter {
         });
     }
 
+    getMaxX(data) {
+        const condition = document.getElementById('condition').value;
+        if (condition === 'simply-supported') {
+            return data.beam.primarySpan;
+        } else if (condition === 'two-span-unequal') {
+            return data.beam.primarySpan + data.beam.secondarySpan;
+        }
+        return data.beam.primarySpan;
+    }
+
     generatePoints(data) {
         const points = [];
         const numPoints = 100;
-        const totalSpan = (data.beam.primarySpan + (data.beam.secondarySpan || 0)) * 1000;
+        const condition = document.getElementById('condition').value;
+
+        // Set total span based on condition
+        let totalSpan;
+        if (condition === 'simply-supported') {
+            totalSpan = data.beam.primarySpan * 1000;
+        } else if (condition === 'two-span-unequal') {
+            totalSpan = (data.beam.primarySpan + data.beam.secondarySpan) * 1000;
+        } else {
+            totalSpan = data.beam.primarySpan * 1000;
+        }
 
         for (let i = 0; i <= numPoints; i++) {
             const x = (i / numPoints) * totalSpan;
